87f9709a03ed05068967f61790099788ec4053b7
zhaodexu <zhdxu@landhightech.com>
2018-10-17 23:04:39 +0800

    git 测试

--- tree/test.py
+++ tree/test.py
@@ -3,8 +3,9 @@
 if __name__ == '__main__':
     map_data = {"key": "val"}
     print map_data
+    print "test ..."
 
     # 测试看看
 
-    del map_data['key1']
+    # del map_data['key1']
     print(map_data)
