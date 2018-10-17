7586d33e81dd14cfebb2176c1590807891536394
zhaodexu <zhdxu@landhightech.com>
2018-10-18 00:09:02 +0800

    Merge branch 'feature/git-test'
    # Conflicts:
    #	tree/test.py

--- tree/test.py
+++ tree/test.py
@@ -6,10 +6,18 @@ if __name__ == '__main__':
 
     print " head content ..."
 
-    print " head content ..."
+    print " head content ... 1"
+
+    print " head content ... 1"
 
     print " head content ..."
 
     print " head content ..."
 
     print " head content ..."
+
+    print " head content ..."
+
+    print " head content ... 1"
+
+    print " head content ... 1"
