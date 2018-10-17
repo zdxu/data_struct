4058894694878a1be3e1378864adea2623cda32a
zhaodexu <zhdxu@landhightech.com>
2018-10-17 23:59:06 +0800

    Merge branch 'feature/git-test'
    # Conflicts:
    #	tree/test.py

--- tree/test.py
+++ tree/test.py
@@ -7,6 +7,11 @@ if __name__ == '__main__':
     # print map_data # test ...
 
     # 测试看看
+    map_data = {"key": "val"}
+    # print map_data # test ...
 
     del map_data['key1']
     print(map_data)
+
+    map_data = {"key": "val"}
+    # print map_data # test ...
